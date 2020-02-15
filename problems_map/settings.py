cd83b880c42ae3cc46508869fcfb902dfe552798
3ANov <jeims0007@gmail.com>
2020-02-15 13:58:45 +0300

    Подготовка для публикации на хероку

--- problems_map/settings.py
+++ problems_map/settings.py
@@ -161,3 +161,7 @@ LEAFLET_CONFIG = {
 GDAL_LIBRARY_PATH = os.getenv('GDAL_LIBRARY_PATH')
 
 GEOS_LIBRARY_PATH = os.getenv('GEOS_LIBRARY_PATH')
+
+# Configure Django App for Heroku.
+import django_heroku
+django_heroku.settings(locals())
