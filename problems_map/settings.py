5660316e4d685cdab6b39e0490b5997a48f6eef3
3ANov <jeims0007@gmail.com>
2020-02-15 13:49:05 +0300

    Подготовка для публикации на хероку

--- problems_map/settings.py
+++ problems_map/settings.py
@@ -162,5 +162,4 @@ GDAL_LIBRARY_PATH = os.getenv('GDAL_LIBRARY_PATH')
 
 GEOS_LIBRARY_PATH = os.getenv('GEOS_LIBRARY_PATH')
 
-db_from_env = dj_database_url.config(conn_max_age=500)
-DATABASES['default'].update(db_from_env)
+DATABASES['default'] = dj_database_url.config(conn_max_age=500, ssl_require=True)
