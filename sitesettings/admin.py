ae543e7ca9eb57f21a6bb259044886b04809662a
3ANov <jeims0007@gmail.com>
2020-02-15 15:40:32 +0300

    Подготовка для публикации на хероку

--- sitesettings/admin.py
+++ sitesettings/admin.py
@@ -1,3 +1,17 @@
 from django.contrib import admin
 
 # Register your models here.
+from sitesettings.models import SiteSettings
+from sitesettings.models import SocialLink
+
+
+class SocialLinkInline(admin.StackedInline):
+    model = SocialLink
+    extra = 1
+
+
+class GeneralSiteSetting(admin.ModelAdmin):
+    inlines = [SocialLinkInline]
+
+
+admin.site.register(SiteSettings,GeneralSiteSetting)
