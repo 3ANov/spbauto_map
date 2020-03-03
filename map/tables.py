d364f53064cfdb826e016d4343a46ef115f79aee
3ANov <jeims0007@gmail.com>
2020-03-03 12:06:19 +0300

    SPBAUTO_MAP-3 Some fix for truncatewords

--- map/tables.py
+++ map/tables.py
@@ -4,7 +4,7 @@ from .models import ProblemLabel
 
 class ProblemsTable(tables.Table):
     description = tables.TemplateColumn(
-        '{{value|truncatewords:10}}')
+        '{{value|truncatewords:9}}')
     class Meta:
         model = ProblemLabel
         fields = ("id", "description", "created_date")
