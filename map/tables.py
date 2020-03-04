1e79ca9302a37dc7f8caef04930d1a5f186d69d4
3ANov <jeims0007@gmail.com>
2020-03-04 15:13:54 +0300

    SPBAUTO_MAP-3 Add column for link(but need create view for problem full description)

--- map/tables.py
+++ map/tables.py
@@ -3,12 +3,12 @@ from .models import ProblemLabel
 
 
 class ProblemsTable(tables.Table):
-    description = tables.TemplateColumn(
-        '{{value|truncatewords:9}}')
+    description = tables.TemplateColumn('{{value|truncatewords:9}}')
+    link = tables.LinkColumn()
+
     class Meta:
         model = ProblemLabel
         fields = ("id", "description", "created_date")
         row_attrs = {
             "bgcolor": lambda record: record.status.color
         }
-
