f4e61e74ecc45decbf6ca9514b02f1200c358e0b
3ANov <3anovdev@gmail.com>
2021-02-08 20:41:36 +0300

    refactor app

--- app/accounts/urls.py
+++ app/accounts/urls.py
@@ -1,16 +1,16 @@
+from django.contrib.auth.views import LoginView, LogoutView, PasswordResetView, PasswordResetDoneView, \
+    PasswordResetConfirmView, PasswordResetCompleteView
 from django.urls import path
-from . import views
+from django_registration.views import RegistrationView
 
 urlpatterns = [
-    path('login/', views.LoginViewCustom.as_view(), name='login'),
-    path('logout/', views.LogoutViewCustom.as_view(), name='logout'),
-    path('password-reset/', views.PasswordResetViewCustom.as_view(), name='password_reset'),
-    path('password-reset/done/', views.PasswordResetDoneViewCustom.as_view(), name='password_reset_done'),
-    path('reset/<uidb64>/<token>/', views.PasswordResetConfirmViewCustom.as_view(), name='password_reset_confirm'),
-    path('reset/done/', views.PasswordResetCompleteViewCustom.as_view(), name='password_reset_complete'),
-
-    path('register/',
-         views.RegistrationViewCustom.as_view(success_url='/'),
+    path('login/', LoginView.as_view(), name='login'),
+    path('logout/', LogoutView.as_view(), name='logout'),
+    path('password-reset/', PasswordResetView.as_view(), name='password_reset'),
+    path('password-reset/done/', PasswordResetDoneView.as_view(), name='password_reset_done'),
+    path('reset/<uidb64>/<token>/', PasswordResetConfirmView.as_view(), name='password_reset_confirm'),
+    path('reset/done/', PasswordResetCompleteView.as_view(), name='password_reset_complete'),
+    path('register/', RegistrationView.as_view(success_url='/'),
          name='django_registration_register'),
 
 ]
--- app/problem_register/urls.py
+++ app/problem_register/urls.py
@@ -7,7 +7,7 @@ from . import views
 urlpatterns = [
     path('', TemplateView.as_view(template_name='problem_register/home.html'), name='home'),
     path('report', views.ReportView.as_view(), name='report'),
-    path('accounts/', include('app.accounts.urls')),
+    path('accounts/', include('accounts.urls')),
     path('problems_set', views.problems_dataset, name='problems_data'),
     path('status_set', views.status_dataset, name='status_data'),
     path("problems_list/", views.ProblemsListView.as_view(), name='problems_list'),
