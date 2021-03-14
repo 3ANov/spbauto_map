d1cd9e9f8e45701f6bdef34eb93b99461e583953
3ANov <3anovdev@gmail.com>
2021-03-14 12:47:57 +0300

    new test to check if issue status has been added

--- app/problem_register/tests.py
+++ app/problem_register/tests.py
@@ -1,8 +1,9 @@
 from django.contrib.gis.geos import Point
+from django.core.management import call_command
 from django.test import TestCase
 from django.utils import timezone
 
-from problem_register.models import ProblemLabel
+from problem_register.models import ProblemLabel, Status
 
 
 class NewProblemReportTest(TestCase):
@@ -19,3 +20,16 @@ class NewProblemReportTest(TestCase):
         problem = ProblemLabel.objects.get(description='Новая тестовая проблема на дороге')
         self.assertEqual(problem.geom.x, 30.333144)
         self.assertEqual(problem.geom.y, 59.931327)
+
+
+class NewStatusReportTest(TestCase):
+    """тест добавления статуса проблемы"""
+
+    def setUp(self):
+        """добавление в базу"""
+        Status.objects.create(name='Новый статус')
+
+    def test_get_status(self):
+        """тест: существует ли статус с таким именем"""
+        status_set = Status.objects.filter(name='Новый статус')
+        self.assertEqual(status_set.count(), 1)
