c04b8d3879eec0291ce6bcb36b4d97c58ef098d9
3ANov <3anovdev@gmail.com>
2021-03-31 21:56:59 +0300

    added address tags + change admin's site

--- app/problem_register/services.py
+++ app/problem_register/services.py
@@ -1,9 +1,14 @@
+import logging
+
 import locationiq
 
 from django.conf import settings
 from locationiq import ApiException
 
+from places.models import Road, StateDistrict, County, Place
 from problem_register.models import ProblemLabel
+
+logger = logging.getLogger(__name__)
 
 
 def add_places_info_for_problem(problem_pk):
@@ -21,8 +26,31 @@ def add_places_info_for_problem(problem_pk):
                                             lon=problem.geom.x,
                                             accept_language='ru',
                                             format='json',
-                                            normalizecity=0,
-                                            addressdetails=1)
-        print(api_response.address)
+                                            normalizecity=1,
+                                            namedetails=1,
+                                            addressdetails=1,
+                                            )
+        logger.info(api_response.address)
+
+        if api_response.address.road:
+            problem.road, _ = Road.objects.get_or_create(name=api_response.address.road)
+
+        if api_response.address.state_district:
+            problem.state_district, _ = StateDistrict.objects.get_or_create(name=api_response.address.state_district)
+
+        if api_response.address.county:
+            problem.county, _ = County.objects.get_or_create(name=api_response.address.county)
+
+        if api_response.address.house_number:
+            problem.house_number = api_response.address.house_number
+
+        if api_response.address.hamlet:
+            problem.place, _ = Place.objects.get_or_create(name=api_response.address.hamlet)
+        elif api_response.address.town:
+            problem.place, _ = Place.objects.get_or_create(name=api_response.address.town)
+        elif api_response.address.state == "Санкт-Петербург":
+            problem.place, _ = Place.objects.get_or_create(name="Санкт-Петербург")
+
+        problem.save()
     except ApiException as e:
-        print("Exception when calling AutocompleteApi->autocomplete: %s\n" % e)
+        print("Exception when calling ReverseApi->reverse: %s\n" % e)
